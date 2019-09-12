c76d3a126b78bbbbca6712f402b8bea5b7309618
Denis Yarats <denisyarats@gmail.com>
2019-09-12 06:27:26 -0700

    fix training

--- train_higher.py
+++ train_higher.py
@@ -8,7 +8,7 @@ from torch import optim
 from torch.optim import lr_scheduler
 import argparse
 
-import torchvision.transforms as transforms
+import torchvision.transforms as tr
 import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
 
@@ -35,6 +35,7 @@ def module_hash(module):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch_size', default=64, type=int)
+    parser.add_argument('--meta_batch_size', default=32, type=int)
     parser.add_argument('--num_epochs', default=300, type=int)
     parser.add_argument('--lr', default=1e-1, type=float)
     parser.add_argument('--momentum', default=0.9, type=float)
@@ -42,146 +43,209 @@ def parse_args():
     parser.add_argument('--seed', default=300, type=int)
     parser.add_argument('--pretrained', default=False, action='store_true')
     parser.add_argument('--work_dir', default='.', type=str)
-    parser.add_argument('--lr_update_frequency', default=100, type=int)
-    parser.add_argument('--num_lr_updates', default=1, type=int)
-    parser.add_argument('--num_lr_inner_steps', default=5, type=int)
-    
+    parser.add_argument('--meta_update_freq', default=1, type=int)
+    parser.add_argument('--meta_num_updates', default=1, type=int)
+    parser.add_argument('--meta_num_inner_steps', default=1, type=int)
 
     args = parser.parse_args()
     return args
 
 
-def make_dataloaders(batch_size=64, num_workers=4, pin_memory=True):
+def make_transform(train=True):
     means = [0.53129727, 0.5259391, 0.52069134]
     stdevs = [0.28938246, 0.28505746, 0.27971658]
-    norm_trans = transforms.Normalize(means, stdevs)
 
-    train_trans = transforms.Compose(
-        [
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(), norm_trans
-        ]
+    transforms = []
+    if train:
+        transforms.append(tr.RandomCrop(32, padding=4))
+        transforms.append(tr.RandomHorizontalFlip())
+
+    transforms.append(tr.ToTensor())
+    transforms.append(tr.Normalize(means, stdevs))
+
+    return tr.Compose(transforms)
+
+
+def make_dataset(train=True):
+    transform = make_transform(train)
+
+    dataset = datasets.CIFAR10(
+        root='cifar10', train=train, download=True, transform=transform
     )
-    test_trans = transforms.Compose([transforms.ToTensor(), norm_trans])
+    return dataset
 
-    kwargs = {'num_workers': num_workers, 'pin_memory': pin_memory}
+
+def make_loaders(batch_size=64):
+    train_dset = make_dataset(train=True)
+    test_dset = make_dataset(train=False)
+
+    kwargs = {'num_workers': 4, 'pin_memory': True}
+
     train_loader = DataLoader(
-        datasets.CIFAR10(
-            root='cifar10', train=True, download=True, transform=train_trans
-        ),
-        batch_size=batch_size,
-        shuffle=True,
-        **kwargs
-    )
-    train_lr_loader = DataLoader(
-        datasets.CIFAR10(
-            root='cifar10', train=True, download=True, transform=train_trans
-        ),
-        batch_size=batch_size,
-        shuffle=True,
-        **kwargs
+        train_dset, batch_size=batch_size, shuffle=True, **kwargs
     )
     test_loader = DataLoader(
-        datasets.CIFAR10(
-            root='cifar10', train=False, download=True, transform=test_trans
-        ),
-        batch_size=batch_size,
-        shuffle=False,
-        **kwargs
+        test_dset, batch_size=batch_size, shuffle=False, **kwargs
     )
 
-    return train_loader, train_lr_loader, test_loader
+    return train_loader, test_loader
 
 
-def train_lr(model, lr, opt, lr_opt, data_loader, num_steps, device):
-    lr_opt.zero_grad()
-    with higher.innerloop_ctx(
-        model,
-        opt,
-        copy_initial_weights=False,
-        track_higher_grads=True,
-    ) as (fmodel, diffopt):
-        for batch_idx, (x, y) in enumerate(data_loader):
-            if batch_idx >= num_steps:
-                break
-            x, y = x.to(device), y.to(device)
-            y_hat = fmodel(x)
-            loss = F.nll_loss(y_hat, y)
-            diffopt.step(loss, override={'lr': lr})
-            
-        param_sum = sum(p.sum() for p in fmodel.parameters())
-        grad = torch.autograd.grad(param_sum, lr)
-        # need to manually set grad 
-        for (g, pg) in zip(grad, lr):
-            pg.grad = g
-    lr_opt.step()
+def make_meta_loaders(batch_size=64, train_ratio=0.8):
+    dataset = make_dataset(train=True)
+
+    train_size = int(len(dataset) * train_ratio)
+    test_size = len(dataset) - train_size
+
+    train_dset, test_dset = torch.utils.data.random_split(
+        dataset, [train_size, test_size]
+    )
+
+    kwargs = {'num_workers': 4, 'pin_memory': True}
+
+    train_loader = DataLoader(
+        train_dset, batch_size=batch_size, shuffle=True, **kwargs
+    )
+    test_loader = DataLoader(
+        test_dset, batch_size=batch_size, shuffle=True, **kwargs
+    )
+
+    return train_loader, test_loader
 
 
-def train(
-    step, epoch, model, lr, data_loader, lr_data_loader, opt, lr_opt,
-    device, lr_update_frequency, num_lr_updates, num_lr_inner_steps, L
-):
-    model.train()
-    start_time = time.time()
-    for x, y in data_loader:
-        step += 1
 
-        if step % lr_update_frequency == 0:
-            for _ in range(num_lr_updates):
-                train_lr(
-                    model, lr, opt, lr_opt, lr_data_loader,
-                    num_lr_inner_steps, device
-                )
-            # set new learning rate for each param group
-            for lr_idx, (pg, g_lr) in enumerate(zip(opt.param_groups, lr)):
-                pg['lr'] = g_lr.item()
-                L.log('train/learning_rate_%d' % lr_idx, g_lr.item())
-                
+class MetaTrainer(object):
+    def __init__(
+        self, model, init_lr, momentum, weight_decay, batch_size,
+        meta_batch_size, meta_update_freq, meta_num_updates,
+        meta_num_inner_steps, device
+    ):
+        super().__init__()
 
-        x, y = x.to(device), y.to(device)
+        self.model = model.to(device)
         
-        opt.zero_grad()
-        y_hat = model(x)
-        loss = F.nll_loss(y_hat, y)
-        loss.backward()
-        opt.step()
+        self.meta_update_freq = meta_update_freq
+        self.meta_num_updates = meta_num_updates
+        self.meta_num_inner_steps = meta_num_inner_steps
+        self.device = device
 
-        prediction = y_hat.max(1)[1]
-        accuracy = prediction.eq(y).sum().item()
+        param_groups = [
+            {
+                'params': p,
+                'lr': init_lr
+            } for p in self.model.parameters()
+        ]
+        self.opt = optim.SGD(
+            param_groups,
+            lr=init_lr,
+            momentum=momentum,
+            weight_decay=weight_decay
+        )
 
-        L.log('train/loss', loss)
-        L.log('train/accuracy', 100. * accuracy / x.shape[0])
+        self.learnable_lr =  higher.optim.get_trainable_opt_params(self.opt, device=self.device)['lr']
+        self.lr_opt = optim.Adam(self.learnable_lr)
 
-        if step % 100 == 0:
-            #L.log('train/learning_rate', scheduler.get_lr()[0])
-            L.log('train/duration', time.time() - start_time)
-            L.log('train/epoch', epoch)
-            L.dump(step)
-            start_time = time.time()
+        self.train_loader, self.test_loader = make_loaders(batch_size)
+        self.meta_train_loader, self.meta_test_loader = make_meta_loaders(
+            meta_batch_size
+        )
 
-    L.log('train/duration', time.time() - start_time)
-    L.log('train/epoch', epoch)
-    L.dump(step)
+    def meta_train_iter(self, step, epoch, L):
+        self.lr_opt.zero_grad()
+        with higher.innerloop_ctx(
+            self.model,
+            self.opt,
+            copy_initial_weights=False,
+            track_higher_grads=True,
+            override={'lr': self.learnable_lr}
+        ) as (fmodel, diffopt):
+            for batch_idx, (x, y) in enumerate(self.meta_train_loader):
+                if batch_idx >= self.meta_num_inner_steps:
+                    break
+                x, y = x.to(self.device), y.to(self.device)
+                y_hat = fmodel(x)
+                loss = F.nll_loss(y_hat, y)
+                diffopt.step(loss)
 
-    return step
+            test_loss = 0
+            for batch_idx, (x, y) in enumerate(self.meta_test_loader):
+                if batch_idx >= self.meta_num_inner_steps:
+                    break
+                x, y = x.to(self.device), y.to(self.device)
+                y_hat = fmodel(x)
+                test_loss += F.nll_loss(y_hat, y)
+            
+            grads = torch.autograd.grad(test_loss, self.learnable_lr)
+            # need to manually set grad
+            for param, grad in zip(self.learnable_lr, grads):
+                param.grad = grad
+        self.lr_opt.step()
 
+        # set new learning rate for each param group
+        for i, (group, lr) in enumerate(
+            zip(self.opt.param_groups, self.learnable_lr)
+        ):
+            group['lr'] = lr.item()
+            L.log('train/learning_rate_%d' % i, lr.item())
 
-def test(step, epoch, model, data_loader, device, L):
-    model.eval()
-    for x, y in data_loader:
-        x, y = x.to(device), y.to(device)
-        with torch.no_grad():
-            y_hat = model(x)
-        loss = F.nll_loss(y_hat, y, reduction='sum')
-        prediction = y_hat.max(1)[1]
-        accuracy = prediction.eq(y).sum().item()
+    def train_iter(self, step, epoch, L):
+        self.model.train()
+        start_time = time.time()
+        for x, y in self.train_loader:
+            step += 1
 
-        L.log('test/loss', loss, n=x.shape[0])
-        L.log('test/accuracy', 100. * accuracy, n=x.shape[0])
+            if step % self.meta_update_freq == 0:
+                for _ in range(self.meta_num_updates):
+                    self.meta_train_iter(step, epoch, L)
 
-    L.log('test/epoch', epoch)
-    L.dump(step)
+            x, y = x.to(self.device), y.to(self.device)
+
+            self.opt.zero_grad()
+            y_hat = self.model(x)
+            loss = F.nll_loss(y_hat, y)
+            loss.backward()
+            self.opt.step()
+
+            prediction = y_hat.max(1)[1]
+            accuracy = prediction.eq(y).sum().item()
+
+            L.log('train/loss', loss)
+            L.log('train/accuracy', 100. * accuracy / x.shape[0])
+
+            if step % 100 == 0:
+                #L.log('train/learning_rate', scheduler.get_lr()[0])
+                L.log('train/duration', time.time() - start_time)
+                L.log('train/epoch', epoch)
+                L.dump(step)
+                start_time = time.time()
+
+        L.log('train/duration', time.time() - start_time)
+        L.log('train/epoch', epoch)
+        L.dump(step)
+
+        return step
+
+    def test_iter(self, step, epoch, L):
+        self.model.eval()
+        for x, y in self.test_loader:
+            x, y = x.to(self.device), y.to(self.device)
+            with torch.no_grad():
+                y_hat = self.model(x)
+            loss = F.nll_loss(y_hat, y, reduction='sum')
+            prediction = y_hat.max(1)[1]
+            accuracy = prediction.eq(y).sum().item()
+
+            L.log('test/loss', loss, n=x.shape[0])
+            L.log('test/accuracy', 100. * accuracy, n=x.shape[0])
+
+        L.log('test/epoch', epoch)
+        L.dump(step)
+
+    def train(self, num_epochs, L):
+        step = 0
+        for epoch in range(1, num_epochs + 1):
+            self.train_iter(step, epoch, L)
+            self.test_iter(step, epoch, L)
 
 
 def main():
@@ -196,7 +260,7 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     model = densenet.DenseNet(
-        growth_rate=32,
+        growth_rate=16,
         depth=100,
         reduction=0.5,
         bottleneck=True,
@@ -204,29 +268,20 @@ def main():
     )
     model = model.to(device)
 
-    param_groups = [{'params': p, 'lr': args.lr} for p in model.parameters()]
-    opt = optim.SGD(
-        param_groups,
-        lr=args.lr,
+    trainer = MetaTrainer(
+        model=model,
+        init_lr=args.lr,
         momentum=args.momentum,
-        weight_decay=args.weight_decay
+        weight_decay=args.weight_decay,
+        batch_size=args.batch_size,
+        meta_batch_size=args.meta_batch_size,
+        meta_update_freq=args.meta_update_freq,
+        meta_num_updates=args.meta_num_updates,
+        meta_num_inner_steps=args.meta_num_inner_steps,
+        device=device
     )
 
-    lr = nn.ParameterList([nn.Parameter(x.to(device)) for x in higher.optim.get_trainable_opt_params(opt)['lr']])
-
-    train_loader, train_lr_loader, test_loader = make_dataloaders(
-        batch_size=args.batch_size
-    )
-
-    lr_opt = optim.Adam(lr)
-
-    step = 0
-    for epoch in range(1, args.num_epochs + 1):
-        step = train(
-            step, epoch, model, lr, train_loader, train_lr_loader, opt, lr_opt,
-            device, args.lr_update_frequency, args.num_lr_updates, args.num_lr_inner_steps, L
-        )
-        test(step, epoch, model, test_loader, device, L)
+    trainer.train(args.num_epochs, L)
 
 
 if __name__ == '__main__':
